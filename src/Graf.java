927af3bbe587903c7df585fbdf71beff17ed38f9
Adam <adam.virostek@gmail.com>
2022-05-04 15:54:39 +0200

    cpm

--- src/Graf.java
+++ src/Graf.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -9,31 +8,29 @@ import java.util.TreeMap;
 public class Graf {
 
     private TreeMap<Integer, Vrchol> vrcholy;
+    private ArrayList<Vrchol> monoton;
+    private int[] p;
+    private int[] k;
+    private int[] z;
 
-    public Graf(String vrcholy, String hrany) throws IOException {
+    public Graf(String vrcholy, String hrany) {
         this.vrcholy = new TreeMap<>();
-
-        try {
-            if (nacitajGraf(vrcholy, hrany) == 1) {
-                throw new IOException();
-            }
-        } catch (IOException ex) {
-            System.err.println("Nepodarilo sa nacitat subor...");
-        }
+        this.nacitajGraf(vrcholy, hrany);
     }
 
-    public int nacitajGraf(String vrcholy, String hrany) {
+    public void nacitajGraf(String vrcholy, String hrany) {
         try {
             File vrcholySubor = new File(vrcholy);
             Scanner s = new Scanner(vrcholySubor);
             while (s.hasNext()) {
                 int vrchol = s.nextInt();
-                int trvanie = s.nextInt();
+                int cas = s.nextInt();
                 if (this.vrcholy.get(vrchol) == null) {
-                    this.vrcholy.put(vrchol, new Vrchol(vrchol, trvanie));
+                    this.vrcholy.put(vrchol, new Vrchol(vrchol, cas));
                 }
             }
             s.close();
+
             File hranySubor = new File(hrany);
             Scanner sc = new Scanner(hranySubor);
             while (sc.hasNext()) {
@@ -43,72 +40,142 @@ public class Graf {
                 this.vrcholy.get(koncVrchol).pridajVchadzajucuHranu(startVrchol);
             }
             sc.close();
-            return 0;
         } catch (IOException ex) {
-            return 1;
+            System.err.println("Nepodarilo sa nacitat subor...");
         }
 
     }
 
-    public void vypisVrcholy() {
-        for (Map.Entry<Integer, Vrchol> vrchols : this.vrcholy.entrySet()) {
-            System.out.printf("Vrchol %d, p: %d, InDeg: %d, OutDeg: %d%n", vrchols.getValue().getId(),
-                    vrchols.getValue().getTrvanie(), vrchols.getValue().getInDeg(), vrchols.getValue().getOutDeg());
-
-        }
+    public void CPM() {
+        int celkoveTrvanie = cpmZaciatky();
+        this.cpmKonce(celkoveTrvanie);
+        this.vypisCpm(celkoveTrvanie);
+        this.kritickeCinnosti();
     }
 
-    public ArrayList<Vrchol> monotonneOcislovanie() {
-        ArrayList<Vrchol> P = new ArrayList<>();
-        P.add(null);
+    private void monotonneOcislovanie() {
+        this.monoton = new ArrayList<>();
+        this.monoton.add(null);
+
         for (Map.Entry<Integer, Vrchol> vrchols : this.vrcholy.entrySet()) {
             if (vrchols.getValue().getInDeg() == 0) {
-                P.add(vrchols.getValue());
+                this.monoton.add(vrchols.getValue());
             }
         }
+
         int i = 1;
-        while (P.size() != this.vrcholy.size() + 1) {
-            int v = P.get(i).getId();
+        while (this.monoton.size() != this.vrcholy.size() + 1) {
+            int v = this.monoton.get(i).getId();
             for (int vrchol : this.vrcholy.get(v).getVystupne()) {
                 this.vrcholy.get(vrchol).odcitajInDeg();
                 if (this.vrcholy.get(vrchol).getInDeg() == 0) {
-                    P.add(this.vrcholy.get(vrchol));
+                    this.monoton.add(this.vrcholy.get(vrchol));
                 }
             }
             i++;
         }
-
-        return P;
     }
 
-    public void cpm() {
-        int[] p = new int[this.vrcholy.size() + 1];
-        int[] z = new int[this.vrcholy.size() + 1];
-        int[] trvanie = new int[this.vrcholy.size() + 1];
+    private void inicializujCPM() {
+        this.p = new int[this.vrcholy.size() + 1];
+        this.z = new int[this.vrcholy.size() + 1];
+        this.k = new int[this.vrcholy.size() + 1];
 
-        ArrayList<Vrchol> monoton = this.monotonneOcislovanie();
+        this.monotonneOcislovanie();
 
         for (int i = 1; i < this.vrcholy.size() + 1; i++) {
-            p[monoton.get(i).getId()] = monoton.get(i).getTrvanie();
-            z[i] = 0;
+            this.p[this.monoton.get(i).getId()] = this.monoton.get(i).getTrvanie();
+        }
+    }
+
+    private int cpmZaciatky() {
+        this.inicializujCPM();
+
+        int[] trvanie = new int[this.vrcholy.size() + 1];
+        int celkoveTrvanie;
+
+        for (int i = 1; i < this.vrcholy.size() + 1; i++) {
+            this.z[i] = 0;
         }
 
-        for (int i = 1; i < monoton.size(); i++) {
-            Vrchol v = monoton.get(i);
-            trvanie[v.getId()] = z[v.getId()] + p[v.getId()];
+        for (int i = 1; i < this.monoton.size(); i++) {
+            Vrchol v = this.monoton.get(i);
+
             for (int j = 0; j < v.getVystupne().size(); j++) {
-                z[v.getVystupne().get(j)] = trvanie[v.getId()];
+                int w = v.getVystupne().get(j);
+                if (z[w] < z[v.getId()] + p[v.getId()]) {
+                    z[w] = z[v.getId()] + p[v.getId()];
+                    trvanie[w] = v.getId();
+                }
             }
         }
 
-        for (int i = 1; i < monoton.size(); i++) {
-            System.out.printf("v: %d, najskor mozny zaciatok: %d%n", monoton.get(i).getId(),
-                    trvanie[monoton.get(i).getId()]);
-        }
-
-        int celkoveTrvanie = trvanie[monoton.get(monoton.size() - 1).getId()];
-        System.out.println("Celkove trvanie projektu: " + celkoveTrvanie);
-
+        int poslednyVrchol = this.monoton.get(this.monoton.size() - 1).getId(); // na konci je vrchol vzdy odeg = 0
+        celkoveTrvanie = z[poslednyVrchol] + p[poslednyVrchol];
+        return celkoveTrvanie;
     }
 
+    private void cpmKonce(int celkoveTrvanie) {
+        int[] trvanie = new int[this.vrcholy.size() + 1];
+
+        for (int i = 1; i < this.vrcholy.size() + 1; i++) {
+            this.k[i] = celkoveTrvanie;
+        }
+
+        for (int i = this.monoton.size() - 1; i > 0; i--) {
+
+            Vrchol v = this.monoton.get(i);
+
+            for (int j = 0; j < v.getVystupne().size(); j++) {
+                int w = v.getVystupne().get(j); // vrchol vo vystupnej hviezde
+
+                if (k[v.getId()] > (k[w] - p[w])) {
+                    k[v.getId()] = k[w] - p[w];
+                    trvanie[v.getId()] = w;
+                }
+            }
+        }
+    }
+
+    private void kritickeCinnosti() {
+        ArrayList<Integer> kritickeCinnosti = new ArrayList<Integer>();
+        for (int i = 1; i < this.vrcholy.size() + 1; i++) {
+            int v = this.vrcholy.get(i).getId();
+            int rezerva = this.k[v] - this.z[v] - this.p[v];
+            if (rezerva == 0) {
+                kritickeCinnosti.add(v);
+            }
+        }
+
+        System.out.print("Kriticke cinnosti su:");
+        for (int i : kritickeCinnosti) {
+            System.out.print(", " + i);
+        }
+        System.out.println();
+    }
+
+    private void vypisCpm(int celkoveTrvanie) {
+        System.out.println("Zaciatky:");
+        for (int i = 1; i < this.monoton.size(); i++) {
+            System.out.printf("v: %2d, z[v]: %4d%n", this.monoton.get(i).getId(),
+                    z[this.monoton.get(i).getId()]);
+        }
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Konce:");
+        for (int i = this.monoton.size() - 1; i > 0; i--) {
+            System.out.printf("v: %2d, k[v]: %4d%n", this.monoton.get(i).getId(),
+                    k[this.monoton.get(i).getId()]);
+        }
+
+        System.out.println("Celkove trvanie projektu: " + celkoveTrvanie);
+    }
+
+    public void vypisVrcholy() {
+        for (Map.Entry<Integer, Vrchol> vrchols : this.vrcholy.entrySet()) {
+            System.out.printf("Vrchol %2d, p: %3d, InDeg: %2d, OutDeg: %2d%n", vrchols.getValue().getId(),
+                    vrchols.getValue().getTrvanie(), vrchols.getValue().getInDeg(), vrchols.getValue().getOutDeg());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Graf graf = new Graf("ATG_DAT/Trvania1.txt", "ATG_DAT/Hrany1.txt");
-        graf.vypisVrcholy();
-        graf.cpm();
+        Graf graf = new Graf("ATG_DAT/Trvania.txt", "ATG_DAT/Hrany.txt");
+        // graf.vypisVrcholy();
+        graf.CPM();
     }
 }
